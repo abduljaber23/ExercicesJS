7cf5d4b974bd21bb2ceda557ad9a73b62aac4feb
Al omran abduljaber <alomranabduljaber@gmail.com>
2025-01-17 14:52:44 +0100

    exercice 2

---
+++ Exercice 2/script.js
@@ -0,0 +1,10 @@
+// A partir d'une variable genre qui peux être soit "male" ou "female"
+// Faire une condition qui va afficher dans la console :
+// "Je suis un homme" OU "je suis une femme"
+
+let genre = "homme";
+if (genre === "homme") {
+  console.log("je suis un homme");
+} else{
+    console.log('je suis une femme');
+}
