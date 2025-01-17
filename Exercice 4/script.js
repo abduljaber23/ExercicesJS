e27ec96e6c2c39eb2c8d88b583e9573476b0c05e
Al omran abduljaber <alomranabduljaber@gmail.com>
2025-01-17 15:29:06 +0100

    exercice 4

---
+++ Exercice 4/script.js
@@ -0,0 +1,12 @@
+// A partir d'une variable age et d'une variable height
+// Faire une condition qui permet de vérifier si la personne a + de 18 ans et mesure + de 140, dans ce cas :
+// Afficher dans la console : "Je peux rentrer dans le manège"
+// Sinon afficher dans la console : "Je n'ai pas le droit de rentrer dans le manège"
+
+let age = 18;
+let height = 140;
+if (age >= 18 && height >= 140) {
+  console.log("Je peux rentrer dans le manège");
+} else {
+  console.log("Je n`ai pas le droit de rentrer dans le manège");
+}
