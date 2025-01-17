237d16e18bb49483b0219e5f8cf90f617f1ffbad
Al omran abduljaber <alomranabduljaber@gmail.com>
2025-01-17 15:08:31 +0100

    exercice 3

---
+++ Exercice 3/script.js
@@ -0,0 +1,18 @@
+// à partir de 3 variables :
+
+// gender (string)
+// height (int)
+// age (int)
+
+
+// En fonction du genre afficher la phrase :
+
+// J'ai {x} ans, je mesure {x} centimètres et suis un homme
+// J'ai {x} ans, je mesure {x} centimètres et suis une femme
+
+let gender = 'homme'
+let height = '171cm'
+let age = 23
+console.log(
+    'J`ai ' + age + ',' + ' je mesure ' + height + ' et je suis un homme'
+)
